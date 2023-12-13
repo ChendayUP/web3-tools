c680d5fc261fca4f8c282fd791573d193cdf98a5
xixinRunBoy <godlikeonline@hotmail.com>
2023-12-13 16:54:58 +0800

    feat: current account status add

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -4,39 +4,50 @@ import { motion } from 'framer-motion'
 
 import { BranchIsWalletConnected } from '@/components/shared/branch-is-wallet-connected'
 import { FADE_DOWN_ANIMATION_VARIANTS } from '@/config/design'
-import { useWalletClient, usePublicClient } from 'wagmi'
+import { useWalletClient, usePublicClient, useAccount } from 'wagmi'
 import { formatEther } from 'viem'
 import { useEffect, useState } from 'react'
 
 interface addressType {
+  orginAddress: string | undefined
   address: string | undefined
   balance: string
 }
 
 export default function PageDashboardAccount() {
+  const { address: currentAddress } = useAccount()
   const { data: client, isError, isLoading } = useWalletClient()
   const publicClient = usePublicClient()
   const [addressList, setAddressList] = useState<addressType[]>([])
 
+  console.log(isError, isLoading)
   const getAddress = async () => {
     if (client) {
-      let list: addressType[] = []
       const address = await client.requestAddresses()
-      for (const addr of address) {
+      const balancePromises = address.map(async (addr) => {
         const balance = await publicClient.getBalance({
           address: addr,
         })
-        list.push({
+        return {
+          orginAddress: addr,
           address: truncateAddress(addr),
           balance: trimFormattedBalance(formatEther(balance)),
-        })
-      }
-      setAddressList(list)
+        }
+      })
+      const updatedAddressList = await Promise.all(balancePromises)
+      setAddressList(updatedAddressList)
     }
   }
   useEffect(() => {
     getAddress()
-  })
+  }, [isLoading])
+
+  const isCurrentAccount = (addr?: string) => {
+    console.log(addr, currentAddress)
+    if (addr === currentAddress) {
+      return <span className="text-blue-700">(current)</span>
+    }
+  }
 
   return (
     <>
@@ -51,8 +62,8 @@ export default function PageDashboardAccount() {
           <>
             {addressList.map((addr) => {
               return (
-                <div className="card w-[300px] mr-4">
-                  <h3 className="text-2xl font-normal">Account</h3>
+                <div className="card w-[300px] mr-4" key={addr.address}>
+                  <h3 className="text-2xl font-normal">Account {isCurrentAccount(addr.orginAddress)}</h3>
                   <hr className="my-3 dark:opacity-30" />
                   <div className="mt-3">
                     <span className="mr-1 font-bold">Address:</span> {addr.address}
